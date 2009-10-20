39eea14469600bdef19ce5f2c1847d7c1b300df1
Alexy Khrabrov <deliverable@gmail.com>
2009-10-19 20:36:11 -0400

    userDays with bitsets, nTwits once per twit count

--- src/main/scala/com/tfitter/trends/trends.scala
+++ src/main/scala/com/tfitter/trends/trends.scala
@@ -1,13 +1,13 @@
 package com.tfitter.trends
 
 import java.util.BitSet
-import org.joda.time.DateTime
+import org.joda.time.{DateTime,Days}
 import org.scala_tools.time.Imports._
 import scala.collection.mutable.{Map,Set}
 
 import org.suffix.util.bdb.{BdbFlags, BdbArgs}
 
-import com.tfitter.db.types._
+import com.tfitter.db.types.UserID // {UserID => User}
 import com.tfitter.db.{Twit,TwitRole,TwitInit,TwitReply,TwitMumble}
 
 import com.tfitter.tokenizer._
@@ -15,57 +15,126 @@ import com.tfitter.corpus.{TVisitor,TwitterCorpus,ActOften}
 import com.tfitter.corpus.RichTokenizedLM._
 import System.err
 
-object types {
-type Word = String
-type People = Set[UserID]
+
 // day of year from 0 to Total_days in study
-type Days = BitSet
+object DaySet {
+    var epoch = new DateTime("2008-01-01")
+}
+class DaySet {
+    val bits = new java.util.BitSet
+    
+    def set(dt: DateTime) = {
+        val eDay = Days.daysBetween(DaySet.epoch,dt).getDays
+        bits.set(eDay)
+    }
+    
+    def indices/*(bits: java.util.BitSet)*/: List[Int] = {
+        def aux(i: Int, li: List[Int]): List[Int] = {
+            val next = bits.nextSetBit(i)
+            if (next < 0) li.reverse
+            else aux(next+1,next::li)
+        }
+        aux(0,List())
+    }
+}
+
+case class WordUserProgress(
+    word: Option[Long],
+    user: Option[Long],
+    pair: Option[Long]
+)
+
+
+object WordInfo {
+    // TODO may be needed for dayPeopleSize array length
+    var totalDays = 50
+}
+
+
+
+case class WordInfo (   
+    var start: DateTime
+) {
+    var end:   DateTime = start
+
+    val days = new DaySet
+    val userDays: types.UserDays = Map()
+    
+    // sizes of userDays per day
+    var dayPeopleSize: Array[Int] = Array()
+    
+    var nTimes:   Long = 0
+    var nTwits:   Long = 0
+    var nInits:   Long = 0
+    var nReplies: Long = 0
+}
+
+
+object types {
+    type Word = String
+    type Users = Set[UserID]
+    type UserDays = Map[UserID,DaySet]
+    type WordTabs = Map[Word,WordInfo]
 }
 import types._
 
 
-case class WordUserProgress(
-	word: Option[Long],
-	user: Option[Long],
-	pair: Option[Long])
-	
-	
 // NB could make WordUserProgress constructor argument
 case class WordPeople(name: String) {
-  val words: Map[Word,People] = Map.empty
-  val users: People = Set.empty
+  val words: WordTabs = Map.empty
+  val users: Users = Set.empty
   var nWords = 0 // == words.size
   var nUsers = 0 // == users.size
   var nWordUsers = 0
 
-  def addUser(token: Word, user: UserID, progress: WordUserProgress): Unit = {
-      	(words get token) match {
-    		case Some(set) => // set += user
-    			// TODO demand notContains in 2.8!  a lá nonEmpty
-    			if (!(set contains user)) { 
-					set += user
-    				nWordUsers += 1
-    				progress.pair match {
-    					case Some(n) if nWordUsers % n == 0 => print(":")
-    					case _ =>
-    				}
-    				if (!(users contains user)) {
-    					users += user
-    					nUsers += 1
-    					progress.user match {
-    						case Some(n) if nUsers % n == 0 => print("o")
-    						case _ =>
-    					}
-    				}
-    			}
-    		case _ => 
-    			words(token) = Set(user)
-    			nWords += 1
-    			progress.word match {
-    				case Some(n) if nWords % n == 0 => print("x")
-    				case _ =>
-    			}
-    	}
+  // this is called when w
+  def addWordUser(info: WordInfo, user: UserID, progress: WordUserProgress) = {
+    info.userDays(user) = new DaySet
+    nWordUsers += 1
+    progress.pair match {
+        case Some(n) if nWordUsers % n == 0 => print(":")
+        case _ =>
+    }
+    if (!(users contains user)) {
+        users += user
+        nUsers += 1
+        progress.user match {
+            case Some(n) if nUsers % n == 0 => print("o")
+            case _ =>
+        }
+    }
+  }
+
+  def addToken(token: Word, t: Twit, newForTwit: Boolean, progress: WordUserProgress): Unit = {
+        val info = 
+        (words get token) match {
+            case Some(info) => 
+                if (!info.userDays.contains(t.uid)) { 
+                    addWordUser(info, t.uid, progress)
+                }
+                info
+            case _ => 
+                val info =  WordInfo(t.time)
+                words(token) = info
+                nWords += 1
+                progress.word match {
+                    case Some(n) if nWords % n == 0 => print("x")
+                    case _ =>
+                }
+                addWordUser(info, t.uid, progress)
+                info
+        }
+
+        if (info.start > t.time) info.start = t.time
+        if (info.end < t.time)   info.end = t.time
+        
+        info.days.set(t.time)
+        info.userDays(t.uid).set(t.time)
+        
+        info.nTimes += 1
+        if (newForTwit) info.nTwits  += 1
+        if (t.isReply) info.nReplies += 1 // may be several per reply!
+        if (t.isInit)  info.nInits   += 1 // may be several per reply!      
   }
   
   override def toString =
@@ -73,24 +142,28 @@ case class WordPeople(name: String) {
 }
 
 
-case class WordInfo(
-	word: String, // reduntant, "just in case"
-	
-	startTime: DateTime,
-	endTime: DateTime,
-	days: Days,
-	
-	nTimes: Long,
-	nTwits: Long,
-	nInits: Long,	
-	nReplies: Long
-)
-
 case class WordRole(name: String) {
   val words = WordPeople(name+" words")
   val users = WordPeople(name+" users")
   val tags  = WordPeople(name+" tags")
   val urls  = WordPeople(name+" urls")
+  
+  
+  def addTwit(t: Twit, progress: WordUserProgress) = {
+    val tzer = new TwitTokens(t.text)
+    val tokensSeen: Set[TwitToken] = Set.empty
+    for (token <- tzer) {
+        val newForTwit = !tokensSeen.contains(token)
+        token match {
+            case TwitTokenUser(w) => users.addToken(w, t, newForTwit, progress)
+            case TwitTokenTag(w)  => tags.addToken(w, t,  newForTwit, progress)
+            case TwitTokenUrl(w)  => urls.addToken(w, t,  newForTwit, progress)
+            case TwitTokenWord(w) => words.addToken(w, t, newForTwit, progress)
+            case TwitTokenNone() => // TODO shouldn't happen?
+        }
+        tokensSeen += token
+    }
+  }
  
   override def toString = name+" : WordRole"+List(words,users,tags,urls).foldLeft("")(_+"\n"+_)
 }
@@ -101,46 +174,33 @@ case class WordMumblers()   extends WordRole("mumblers")
 case class WordTweeters()   extends WordRole("tweeters")
 
 abstract class TVisitorWordsBase(
-	val lowerCase: Boolean,
-	val twitProgress: Option[Long],
-	val progress: WordUserProgress) extends TVisitor {
-	
+    val lowerCase: Boolean,
+    val twitProgress: Option[Long],
+    val progress: WordUserProgress) extends TVisitor {
+    
   var everySoOften: List[ActOften] = List()
-	
-  def addTwitRole(wr: WordRole, t: Twit) = {
-    val tzer = new TwitTokens(t.text) 
-    for (token <- tzer) {
-		token match {
-			case TwitTokenUser(w) => wr.users.addUser(w, t.uid, progress)
-			case TwitTokenTag(w)  => wr.tags.addUser(w, t.uid,  progress)
-			case TwitTokenUrl(w)  => wr.urls.addUser(w, t.uid,  progress)
-			case TwitTokenWord(w) => wr.words.addUser(w, t.uid, progress)
-			case TwitTokenNone() => // TODO shouldn't happen?
-		}
-    }
-  }
-  
+      
   // doTwit still undefined, see derived!
   def toString: String
 }
 
 
 class TVisitorWordsByRole(
-	override val lowerCase: Boolean,
-	override val twitProgress: Option[Long],
-	override val progress: WordUserProgress) extends TVisitorWordsBase(
-		lowerCase, twitProgress, progress) {
+    override val lowerCase: Boolean,
+    override val twitProgress: Option[Long],
+    override val progress: WordUserProgress) extends TVisitorWordsBase(
+        lowerCase, twitProgress, progress) {
 
   val initiators = WordInitiators()
   val repliers   = WordRepliers()
   val mumblers   = WordMumblers()
   
   def doTwit(t: Twit) = {
-  	t.role match {
-  		case TwitInit(_)    => addTwitRole(initiators, t)
-  		case TwitReply(_,_) => addTwitRole(repliers, t)
-  		case TwitMumble()   => addTwitRole(mumblers, t)
-  	}
+    t.role match {
+        case TwitInit(_)    => initiators.addTwit(t, progress)
+        case TwitReply(_,_) => repliers.addTwit(t, progress)
+        case TwitMumble()   => mumblers.addTwit(t, progress)
+    }
   }
   
   // def joinRoles: WordTweeters
@@ -150,17 +210,17 @@ class TVisitorWordsByRole(
 
 
 class TVisitorWordsTogether(
-	override val lowerCase: Boolean,
-	override val twitProgress: Option[Long],
-	override val progress: WordUserProgress) extends TVisitorWordsBase(
-		lowerCase, twitProgress, progress) {
+    override val lowerCase: Boolean,
+    override val twitProgress: Option[Long],
+    override val progress: WordUserProgress) extends TVisitorWordsBase(
+        lowerCase, twitProgress, progress) {
 
-	val tweeters = WordTweeters()
+    val tweeters = WordTweeters()
 
-	// could make addTwitRole curried
-	def doTwit(t: Twit) = addTwitRole(tweeters, t)
-	
-	override def toString = "words together: "+tweeters
+    // could make addTwitRole curried
+    def doTwit(t: Twit) = tweeters.addTwit(t, progress)
+    
+    override def toString = "words together: "+tweeters
 }
 
 object WordUsers extends optional.Application {
@@ -207,15 +267,15 @@ object WordUsers extends optional.Application {
     val twitCorpus = new TwitterCorpus(bdbArgs)
           
     val tv = byRole match {
-    	case Some(b) if b => new TVisitorWordsByRole(
-									  lowerCase_,
-									  twitProgress,
-									  wordUserProgress)
-		case _            => new TVisitorWordsTogether(
-									  lowerCase_,
-									  twitProgress,
-									  wordUserProgress)
-		}		
+        case Some(b) if b => new TVisitorWordsByRole(
+                                      lowerCase_,
+                                      twitProgress,
+                                      wordUserProgress)
+        case _            => new TVisitorWordsTogether(
+                                      lowerCase_,
+                                      twitProgress,
+                                      wordUserProgress)
+        }       
     
     maxTwits match {
       case Some(atMost) =>
